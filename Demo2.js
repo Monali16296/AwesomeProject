95a2454f1ad7221bfb8d59d3d42645ab1074e869
Monali Hingu <monali16296@gmail.com>
2019-05-11 20:07:36 +0530

    Image component

---
+++ Demo2.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Image, Text, View } from 'react-native';
+
+export default class Demo2 extends Component {
+    render() {
+        let pic= {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return (
+            <View>
+                <Image source={pic} style={{ width: 193, height: 110 }} />
+                <Text>oo</Text>
+            </View>
+        );
+    }
+}
--- Main.js
+++ Main.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import App from './App';
 import Demo1 from './Demo1';
-
+import Demo2 from './Demo2';
 
 export default class Main extends Component {
     render() {
         return (
             <View>
-             <App />
-            <Demo1 />
+                <App />
+                <Demo1 />
+                <Demo2 />
             </View>
-            );
+        );
     }
 }
