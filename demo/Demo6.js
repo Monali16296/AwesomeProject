76cf24f26438c6d1fefd4a075ed8ff5daf9bfd35
Monali Hingu <monali16296@gmail.com>
2019-05-12 11:45:57 +0530

    Global style

---
+++ demo/Demo6.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Demo6 extends Component {
+    render() {
+        return (
+            <View>
+                <MyAppHeaderText>Text styled as a header</MyAppHeaderText>
+            </View>
+        );
+    }
+}
+
+class MyAppHeaderText extends Component {
+    render() {
+        return (
+            <Text style={{fontSize: 20}}> {this.props.children}</Text>
+        );
+    }
+}
+
+
