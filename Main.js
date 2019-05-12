a798f2b0f3e626990b3d0bceab54e2648252f282
Monali Hingu <monali16296@gmail.com>
2019-05-12 09:51:27 +0530

    numberoflines  is used to define the maximum number of lines of a string that will be rendered before truncation occurs.

--- Main.js
+++ Main.js
@@ -6,6 +6,7 @@ import Demo2 from './Demo2';
 import Demo3 from './demo/Demo3';
 import Demo4 from './demo/Demo4';
 import Demo5 from './demo/Demo5';
+import TextInANest from './demo/TextInANest';
 
 export default class Main extends Component {
     render() {
@@ -17,6 +18,7 @@ export default class Main extends Component {
                 <Demo3 />
                 <Demo4 />
                 <Demo5 />
+                <TextInANest />
             </View>
         );
     }
